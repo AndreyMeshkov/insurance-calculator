bf9b7382097905fd4ff59099faaad596e148c180
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-21 23:50:34 +0300

    feat: add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {HealthComponent} from './health/health.component';
+import {PropertyComponent} from './property/property.component';
+import {TransportComponent} from './transport/transport.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'health', component: HealthComponent},
+  {path: 'property', component: PropertyComponent},
+  {path: 'transport', component: TransportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TransportComponent } from './transport/transport.component';
+import { HealthComponent } from './health/health.component';
+import { PropertyComponent } from './property/property.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TransportComponent,
+    HealthComponent,
+    PropertyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
